dd51e6cde8c38f9cd099ed61799a973c25119c93
Kamil Samigullin <kamil@samigullin.info>
2018-11-02 09:35:07 +0300

    first iteration solution for stepik/course-1547/lesson-41234/step-3 (Failed test #16. Wrong answer)

--- stepik/course-1547/lesson-41234/step-3/main.go
+++ stepik/course-1547/lesson-41234/step-3/main.go
@@ -1,1 +1,70 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+	_ = scanner.Scan()
+	size, _ := strconv.Atoi(scanner.Text())
+	_ = scanner.Scan()
+	count, _ := strconv.Atoi(scanner.Text())
+	traffic := make([][2]int, 0, count)
+	for i := 0; scanner.Scan() && i < count; i++ {
+		arrival, _ := strconv.Atoi(scanner.Text())
+		_ = scanner.Scan()
+		duration, _ := strconv.Atoi(scanner.Text())
+		traffic = append(traffic, [2]int{arrival, duration})
+	}
+	for _, ts := range process(traffic, size) {
+		fmt.Println(ts)
+	}
+}
+
+func process(traffic [][2]int, capacity int) []int {
+	if len(traffic) == 0 {
+		return nil
+	}
+
+	type packet struct {
+		start, end int
+		buffer     int
+	}
+	result := make([]int, 0, len(traffic))
+	flow := make([]packet, 0, len(traffic))
+
+	for len(traffic) > 0 {
+		arrival, duration := traffic[0][0], traffic[0][1]
+		traffic = traffic[1:]
+
+		if len(flow) == 0 {
+			flow = append(flow, packet{arrival, arrival + duration, 1})
+			continue
+		}
+
+		head := flow[len(flow)-1]
+		if arrival >= head.end {
+			flow = append(flow, packet{arrival, arrival + duration, head.buffer})
+			continue
+		}
+
+		if head.buffer < capacity {
+			flow = append(flow, packet{head.end, head.end + duration, head.buffer + 1})
+			continue
+		}
+		flow = append(flow, packet{-1, head.end, head.buffer})
+	}
+
+	for len(flow) > 0 {
+		var tail packet
+		tail, flow = flow[0], flow[1:]
+		result = append(result, tail.start)
+	}
+
+	return result
+}
--- stepik/course-1547/lesson-41234/step-3/main_test.go
+++ stepik/course-1547/lesson-41234/step-3/main_test.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestProcess(t *testing.T) {
+	tests := []struct {
+		name     string
+		traffic  [][2]int
+		capacity int
+		expected []int
+	}{
+		{"case#01", [][2]int{}, 1, nil},
+		{"case#02", [][2]int{{0, 0}}, 1, []int{0}},
+		{"case#03", [][2]int{{0, 1}, {0, 1}}, 1, []int{0, -1}},
+		{"case#04", [][2]int{{0, 1}, {1, 1}}, 1, []int{0, 1}},
+		{"case#05", [][2]int{{0, 1}}, 1, []int{0}},
+		{"discussion#376300", [][2]int{{0, 7}, {0, 0}, {2, 0}, {3, 3}, {4, 0}, {5, 0}}, 3, []int{0, 7, 7, -1, -1, -1}},
+	}
+	for _, test := range tests {
+		tc := test
+		t.Run(test.name, func(t *testing.T) {
+			if obtained := process(tc.traffic, tc.capacity); !reflect.DeepEqual(tc.expected, obtained) {
+				t.Errorf("expected %d, obtained %d", tc.expected, obtained)
+			}
+		})
+	}
+}
