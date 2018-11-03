c81b6f5f82564886e7214258d7b615ad9234bf13
Kamil Samigullin <kamil@samigullin.info>
2018-11-03 08:50:58 +0300

    solve stepik/course-1547/lesson-41234/step-4

--- stepik/course-1547/lesson-41234/step-4/main.go
+++ stepik/course-1547/lesson-41234/step-4/main.go
@@ -1,4 +1,85 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+	opMax  = "max"
+	opPop  = "pop"
+	opPush = "push "
+)
+
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanLines)
+	_ = scanner.Scan()
+	count, _ := strconv.Atoi(scanner.Text())
+	commands := make([]string, 0, count)
+	for i := 0; scanner.Scan() && i < count; i++ {
+		commands = append(commands, scanner.Text())
+	}
+	for _, max := range New(count).process(commands) {
+		fmt.Println(max)
+	}
+}
+
+// New returns new instance of stack with maximums.
+func New(capacity int) *stack {
+	return &stack{data: make([]int, 0, capacity), max: make([]int, 0, capacity)}
+}
+
+type stack struct {
+	data []int
+	max  []int
+}
+
+func (stack *stack) process(commands []string) []int {
+	maximums := make([]int, 0, len(commands)/10)
+	for _, command := range commands {
+		command = strings.ToLower(command)
+		switch {
+		case command == opMax:
+			max, found := stack.maximum()
+			if found {
+				maximums = append(maximums, max)
+			}
+		case command == opPop:
+			_, _ = stack.pop()
+		case strings.HasPrefix(command, opPush):
+			el, _ := strconv.Atoi(strings.TrimPrefix(command, opPush))
+			stack.push(el)
+		}
+	}
+	return maximums
+}
+
+func (stack *stack) push(el int) {
+	max, found := stack.maximum()
+	if !found || (found && max < el) {
+		max = el
+	}
+	stack.max = append(stack.max, max)
+	stack.data = append(stack.data, el)
+}
+
+func (stack *stack) pop() (el int, found bool) {
+	if len(stack.data) == 0 {
+		return
+	}
+	el, stack.data, stack.max = stack.data[len(stack.data)-1], stack.data[:len(stack.data)-1], stack.max[:len(stack.data)-1]
+	return el, true
+}
+
+func (stack *stack) maximum() (max int, found bool) {
+	// stack.data and stack.max must be consistent
+	// panic if not (index out of range)
+	if len(stack.data) == 0 {
+		return
+	}
+	return stack.max[len(stack.data)-1], true
 }
--- stepik/course-1547/lesson-41234/step-4/main_test.go
+++ stepik/course-1547/lesson-41234/step-4/main_test.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestStack(t *testing.T) {
+	tests := []struct {
+		name     string
+		commands []string
+		expected []int
+	}{
+		{"case#01", []string{"push 1", "push 7", "pop"}, []int{}},
+		{"case#02", []string{"push 2", "push 1", "max", "pop", "max"}, []int{2, 2}},
+		{"case#03", []string{"push 7", "push 1", "push 7", "max", "pop", "max"}, []int{7, 7}},
+		{"case#04", []string{"push 1", "push 2", "max", "pop", "max"}, []int{2, 1}},
+		{"case#05", []string{"push 2", "push 3", "push 9", "push 7", "push 2", "max", "max", "max", "pop", "max"},
+			[]int{9, 9, 9, 9}},
+	}
+	for _, test := range tests {
+		tc := test
+		t.Run(test.name, func(t *testing.T) {
+			if obtained := New(len(tc.commands)).process(tc.commands); !reflect.DeepEqual(tc.expected, obtained) {
+				t.Errorf("expected %d, obtained %d", tc.expected, obtained)
+			}
+		})
+	}
+}
