22bb4dac678e75fdf6d37adfb289e5af8e9d266e
Kamil Samigullin <kamil@samigullin.info>
2018-11-04 18:17:49 +0300

    update solution stepik/course-156/lesson-12555/step-11

--- stepik/course-156/lesson-12555/step-11/main.go
+++ stepik/course-156/lesson-12555/step-11/main.go
@@ -2,45 +2,41 @@ package main
 
 import (
 	"bufio"
-	"flag"
 	"fmt"
-	"io"
 	"math"
 	"os"
 	"strconv"
 	"strings"
 )
 
-var test = flag.Bool("test", false, "use test input")
-
-var testCases = `75 2 109`
-
 func main() {
-	var input io.Reader = os.Stdin
-	if flag.Parse(); *test {
-		input = strings.NewReader(testCases)
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+	for scanner.Scan() {
+		n, _ := strconv.Atoi(scanner.Text())
+		fmt.Println(strings.Join(split(n), " "))
 	}
-	scan := bufio.NewScanner(input)
-	scan.Split(bufio.ScanWords)
-	for scan.Scan() {
-		n, _ := strconv.Atoi(scan.Text())
-		result := make([]string, 0, 64)
-		for n > 1 {
-			if simple(n) {
-				result = append(result, strconv.Itoa(n))
-				break
-			}
-			for i, limit := 2, int(math.Sqrt(float64(n))); i <= limit; i++ {
-				if n%i != 0 || !simple(i) {
-					continue
-				}
-				result = append(result, strconv.Itoa(i))
-				n /= i
-				break
-			}
+}
+
+func split(n int) []string {
+	result := make([]string, 0, 64)
+
+	for n > 1 {
+		if simple(n) {
+			result = append(result, strconv.Itoa(n))
+			break
 		}
-		fmt.Println(strings.Join(result, " "))
+		for i, limit := 2, int(math.Sqrt(float64(n))); i <= limit; i++ {
+			if n%i != 0 || !simple(i) {
+				continue
+			}
+			result = append(result, strconv.Itoa(i))
+			n /= i
+			break
+		}
 	}
+
+	return result
 }
 
 func simple(n int) bool {
--- stepik/course-156/lesson-12555/step-11/main_test.go
+++ stepik/course-156/lesson-12555/step-11/main_test.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+var format = `
+expected %+v
+obtained %+v`
+
+func TestSplit(t *testing.T) {
+	tests := []struct {
+		name     string
+		number   int
+		expected []string
+	}{
+		{"case#01", 75, []string{"3", "5", "5"}},
+		{"case#02", 2, []string{"2"}},
+		{"case#03", 109, []string{"109"}},
+	}
+	for _, test := range tests {
+		tc := test
+		t.Run(test.name, func(t *testing.T) {
+			if obtained := split(tc.number); !reflect.DeepEqual(tc.expected, obtained) {
+				t.Errorf(format, tc.expected, obtained)
+			}
+		})
+	}
+}
