c4c34b6acc989c4197d8635e1cdf47305e058db3
Kamil Samigullin <kamil@samigullin.info>
2018-11-04 14:58:55 +0300

    solve stepik/course-1547/lesson-41560/step-1

--- stepik/course-1547/lesson-41560/step-1/main.go
+++ stepik/course-1547/lesson-41560/step-1/main.go
@@ -33,5 +33,35 @@ func (heap *heap) build(numbers []int) [][]string {
 	swaps := make([][]string, 0, len(numbers))
 	heap.data = make([]int, len(numbers))
 	copy(heap.data, numbers)
+	for i := int(len(heap.data) / 2); i >= 0; i-- {
+		swaps = append(swaps, heap.siftDown(i)...)
+	}
 	return swaps
+}
+
+func (heap *heap) siftDown(i int) [][]string {
+	max := i
+	l := left(i)
+	if l < len(heap.data) && heap.data[l] < heap.data[max] {
+		max = l
+	}
+	r := right(i)
+	if r < len(heap.data) && heap.data[r] < heap.data[max] {
+		max = r
+	}
+	if i != max {
+		heap.data[i], heap.data[max] = heap.data[max], heap.data[i]
+		swap := make([][]string, 0, 4)
+		swap = append(swap, []string{strconv.FormatInt(int64(i), 10), strconv.FormatInt(int64(max), 10)})
+		return append(swap, heap.siftDown(max)...)
+	}
+	return nil
+}
+
+func left(i int) int {
+	return 2*i + 1
+}
+
+func right(i int) int {
+	return 2 * (i + 1)
 }
