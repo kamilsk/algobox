bab3a9813178b9d9a7ec9f9db04c63739057b962
Kamil Samigullin <kamil@samigullin.info>
2018-11-05 08:33:03 +0300

    update stepik/course-156/lesson-12556/step-7

--- stepik/course-156/lesson-12556/step-7/main.go
+++ stepik/course-156/lesson-12556/step-7/main.go
@@ -2,53 +2,31 @@ package main
 
 import (
 	"bufio"
-	"flag"
 	"fmt"
-	"io"
 	"os"
 	"strconv"
 	"strings"
 )
 
-var test = flag.Bool("test", false, "use test input")
-
-var testCases = `3
-10 20 30
-3
-9 15 35
-
-10
-0 10 20 30 40   50 60 70 80   90
-5
--1 11 40 41 92`
-
 func main() {
-	var input io.Reader = os.Stdin
-	if flag.Parse(); *test {
-		input = strings.NewReader(testCases)
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+	_ = scanner.Scan()
+	n, _ := strconv.Atoi(scanner.Text())
+	sorted := make([]int, n)
+	for i := 0; i < n && scanner.Scan(); i++ {
+		sorted[i], _ = strconv.Atoi(scanner.Text())
 	}
-	scan := bufio.NewScanner(input)
-	scan.Split(bufio.ScanWords)
-	for scan.Scan() {
-		size, _ := strconv.Atoi(scan.Text())
-		sorted := make([]int, size)
-		for i := 0; i < size; i++ {
-			scan.Scan()
-			sorted[i], _ = strconv.Atoi(scan.Text())
-		}
-		scan.Scan()
-		size, _ = strconv.Atoi(scan.Text())
-		out := make([]string, size)
-		for i := 0; i < size; i++ {
-			scan.Scan()
-			what, _ := strconv.Atoi(scan.Text())
-			out[i] = strconv.Itoa(search(sorted, what))
-		}
-		fmt.Println(strings.Join(out, " "))
+	_ = scanner.Scan()
+	m, _ := strconv.Atoi(scanner.Text())
+	find := make([]int, m)
+	for i := 0; i < m && scanner.Scan(); i++ {
+		find[i], _ = strconv.Atoi(scanner.Text())
 	}
+	fmt.Println(strings.Join(search(sorted, find), " "))
 }
 
-func search(where []int, what int) (nearest int) {
+func search(where []int, what []int) []string {
 	min := func(int1, int2 int) int {
 		if int1 < int2 {
 			return int1
@@ -56,7 +34,7 @@ func search(where []int, what int) (nearest int) {
 		return int2
 	}
 
-	bsearch := func(slice []int, what int) (nearest int) {
+	binary := func(slice []int, what int) (nearest int) {
 		left, right := 0, len(slice)-1
 		for left+1 < right {
 			middle := left + (right-left)/2
@@ -72,33 +50,20 @@ func search(where []int, what int) (nearest int) {
 		return right
 	}
 
-	lsearch := func(slice []int, what int) (nearest int) {
-		for i, end := 0, len(slice); i < end; i++ {
-			if what < slice[i] {
-				if prev := i - 1; prev > -1 && what-slice[prev] <= slice[i]-what {
-					return prev
-				}
-				return i
+	result := make([]string, 0, len(what))
+	for len(what) > 0 {
+		start, end, power := 0, len(where)-1, uint(2)
+		for start < end {
+			last := min(1<<power, end)
+			if what[0] < where[last] {
+				end = start + binary(where[start:last+1], what[0])
+				break
 			}
+			power++
+			start = last
 		}
-		panic("unreachable")
+		result = append(result, strconv.FormatInt(int64(end), 10))
+		what = what[1:]
 	}
-
-	start, end, power := 0, len(where)-1, uint(2)
-	for start < end {
-		last := min(1<<power, end)
-		if what < where[last] {
-			if *test {
-				l, b := lsearch(where[start:last+1], what), bsearch(where[start:last+1], what)
-				if l != b {
-					panic(fmt.Errorf("%d != %d : %+v.find(%d)", l, b, where, what))
-				}
-				return start + b
-			}
-			return start + bsearch(where[start:last+1], what)
-		}
-		power++
-		start = last
-	}
-	return end
+	return result
 }
--- stepik/course-156/lesson-12556/step-7/main_test.go
+++ stepik/course-156/lesson-12556/step-7/main_test.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+var format = `
+expected %+v
+obtained %+v`
+
+func TestSearch(t *testing.T) {
+	tests := []struct {
+		name     string
+		sorted   []int
+		find     []int
+		expected []string
+	}{
+		{"case#01", []int{10, 20, 30}, []int{9, 15, 35}, []string{"0", "0", "2"}},
+		{"case#02", []int{0, 10, 20, 30, 40, 50, 60, 70, 80, 90}, []int{-1, 11, 40, 41, 92},
+			[]string{"0", "1", "4", "4", "9"}},
+	}
+	for _, test := range tests {
+		tc := test
+		t.Run(test.name, func(t *testing.T) {
+			if obtained := search(tc.sorted, tc.find); !reflect.DeepEqual(tc.expected, obtained) {
+				t.Errorf(format, tc.expected, obtained)
+			}
+		})
+	}
+}
