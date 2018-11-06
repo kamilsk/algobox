51bb17f8b3e2439035a767062e74412f276b1075
Kamil Samigullin <kamil@samigullin.info>
2018-11-06 16:11:07 +0300

    work with stepik/course-156/lesson-12560/step-7

--- stepik/course-156/lesson-12560/step-7/main.go
+++ stepik/course-156/lesson-12560/step-7/main.go
@@ -1,4 +1,145 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+	_ = scanner.Scan()
+	n, _ := strconv.Atoi(scanner.Text())
+	fruit := make([]weight, 0, n)
+	for i := 0; i < n && scanner.Scan(); i++ {
+		w, _ := strconv.Atoi(scanner.Text())
+		fruit = append(fruit, weight(w))
+	}
+	_ = scanner.Scan()
+	k, _ := strconv.Atoi(scanner.Text())
+	fmt.Println(greedy{weight(k), new(basket).with(n).insert(fruit...)}.eat())
+}
+
+type weight int
+
+type basket struct {
+	fruit []weight
+	head  int
+}
+
+func (basket *basket) with(capacity int) *basket {
+	basket.fruit, basket.head = make([]weight, 0, capacity), 0
+	return basket
+}
+
+func (basket *basket) insert(fruit ...weight) *basket {
+	// assert(len(fruit) <= cap(basket.fruit) - len(basket.fruit))
+	basket.fruit = append(basket.fruit, fruit...)
+	return basket.sort()
+}
+
+func (basket *basket) empty() bool {
+	return len(basket.fruit) == 0 || basket.fruit[0] == 0
+}
+
+func (basket *basket) sort() *basket {
+	// build heap
+	for i := len(basket.fruit) / 2; i >= 0; i-- {
+		siftDown(basket.fruit, i)
+	}
+	// heap sort
+	process := basket.fruit[1:]
+	for len(process) > 0 {
+		siftDown(process, 0)
+		process = process[1:]
+	}
+	// remove stumps
+	for i := len(basket.fruit) - 1; i >= 0; i-- {
+		if basket.fruit[i] > 0 {
+			break
+		}
+		basket.fruit = basket.fruit[:i]
+	}
+	return basket
+}
+
+func (basket *basket) take() weight {
+	if basket.head < len(basket.fruit) {
+		fruit := basket.fruit[basket.head]
+		basket.head++
+		return fruit
+	}
+	return 0
+}
+
+func (basket *basket) back(fruit ...weight) {
+	// assert(basket.head - len(fruit) == 0)
+	basket.head = 0
+	copy(basket.fruit, fruit)
+	basket.sort()
+}
+
+type greedy struct {
+	force  weight
+	basket interface {
+		empty() bool
+		take() weight
+		back(fruit ...weight)
+	}
+}
+
+func (greedy greedy) eat() int {
+	var steps int
+	for !greedy.basket.empty() {
+		steps++
+		portion, w := make([]weight, 0, 4), weight(0)
+		for w < greedy.force {
+			f := greedy.basket.take()
+			if f == 0 {
+				break
+			}
+			w += f
+			portion = append(portion, f)
+		}
+		w = greedy.force
+		for i := range portion {
+			w -= portion[i]
+			if w < 0 {
+				break
+			}
+			if portion[i] == 1 {
+				portion[i] = 0
+				continue
+			}
+			portion[i] = weight(0.5 + (float32(portion[i]) / 2))
+		}
+		greedy.basket.back(portion...)
+	}
+	return steps
+}
+
+func siftDown(data []weight, i int) {
+	max := i
+	l := left(i)
+	if l < len(data) && data[l] > data[max] {
+		max = l
+	}
+	r := right(i)
+	if r < len(data) && data[r] > data[max] {
+		max = r
+	}
+	if i != max {
+		data[i], data[max] = data[max], data[i]
+		siftDown(data, max)
+	}
+}
+
+func left(i int) int {
+	return 2*i + 1
+}
+
+func right(i int) int {
+	return 2 * (i + 1)
 }
--- stepik/course-156/lesson-12560/step-7/main_test.go
+++ stepik/course-156/lesson-12560/step-7/main_test.go
@@ -1,1 +1,27 @@
 package main
+
+import "testing"
+
+func TestGreedy(t *testing.T) {
+	tests := []struct {
+		name     string
+		cap      int
+		f        weight
+		fruit    []weight
+		expected int
+	}{
+		//{"case#01", 3, 2, []weight{1, 2, 2}, 4},
+		//{"case#02", 3, 6, []weight{4, 3, 5}, 5},
+		//{"case#03", 7, 3, []weight{1, 1, 1, 1, 1, 1, 1}, 3},
+		//{"case#04", 2, 6, []weight{4, 1}, 3},
+		{"case#05", 10, 10, []weight{1, 10, 2, 9, 3, 8, 4, 7, 5, 6}, 12},
+	}
+	for _, test := range tests {
+		tc := test
+		t.Run(test.name, func(t *testing.T) {
+			if obtained := (greedy{tc.f, new(basket).with(tc.cap).insert(tc.fruit...)}).eat(); tc.expected != obtained {
+				t.Errorf("expected %d, obtained %d", tc.expected, obtained)
+			}
+		})
+	}
+}
