12267055b4172d75aa0ba7b6bb1c4395b4b3eb73
Kamil Samigullin <kamil@samigullin.info>
2018-11-08 11:48:28 +0300

    improve tests for stepik/course-217/lesson-13229/step-5

--- stepik/course-217/lesson-13229/step-5/solution_test.py
+++ stepik/course-217/lesson-13229/step-5/solution_test.py
@@ -4,5 +4,9 @@ from solution import gcd
 
 class Test(TestCase):
     def test_gcd(self):
-        self.assertEqual(gcd(18, 35), 1)
-        self.assertEqual(gcd(14159572, 63967072), 4)
+        tests = [
+            {'name': 'case#01', 'a': 18, 'b': 35, 'expected': 1},
+            {'name': 'case#02', 'a': 14159572, 'b': 63967072, 'expected': 4},
+        ]
+        for test in tests:
+            self.assertEqual(gcd(test['a'], test['b']), test['expected'])
