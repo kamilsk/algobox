bdfae10ec5a0697345e4b4969fd2023a1a06f500
Kamil Samigullin <kamil@samigullin.info>
2018-12-05 23:33:16 +0300

    little improvements

--- hackerrank/30-days-of-code/30-hello-world/solution.java
+++ hackerrank/30-days-of-code/30-hello-world/solution.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Solution {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
--- hackerrank/30-days-of-code/30-hello-world/solution.php
+++ hackerrank/30-days-of-code/30-hello-world/solution.php
@@ -1,4 +1,5 @@
 <?php
+
 $handle = fopen('php://stdin', 'r');
 echo 'Hello, World.', PHP_EOL, fgets($handle), PHP_EOL;
 fclose($handle);
