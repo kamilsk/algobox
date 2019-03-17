e2a889ebda350d576c6c75032c56044d32ad8074
Kamil Samigullin <kamil@samigullin.info>
2019-03-17 10:27:25 +0300

    solve seventh challenge for second week of algorithmic toolbox course

---
+++ coursera/algorithmic-toolbox/week-2/algorithmic-warm-up/7_last_digit_of_the_sum_of_fibonacci_numbers_again/solution.py
@@ -0,0 +1,40 @@
+# python3
+def fibonacci_partial_sum_naive(m: int, n: int):
+    total = 0
+
+    previous, current = 0, 1
+    for i in range(n + 1):
+        if i >= m:
+            total += previous
+        previous, current = current, previous + current
+
+    return total % 10
+
+
+def get_fibonacci_huge(n: int, m: int):
+    pisano = list()
+    pisano.append(0)
+    pisano.append(1)
+
+    previous, current, last = 0, 1, 2
+    for i in range(n - 1):
+        previous, current = current, previous + current
+        pisano.append(current % m)
+        for step in range(last, len(pisano) // 2):
+            if pisano[:step] == pisano[step:2 * step]:
+                return pisano[n % step]
+            last = step
+
+    return current % m
+
+
+def fibonacci_partial_sum(m: int, n: int):
+    # sum(fm-1) = fm+1 - f1
+    # sum(fn) = fn+2 - f1
+    # partial sum(fm...fn) = fn+2 - fm+1
+    subset, full = get_fibonacci_huge(m + 1, 10), get_fibonacci_huge(n + 2, 10)
+    return full - subset if full > subset else (10 - subset + full) % 10
+
+
+if __name__ == '__main__':
+    print(fibonacci_partial_sum(*map(int, input().split())))
