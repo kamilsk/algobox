2f943c2e6f6ad96ae342d7acde8d1d7b3ed8f88a
Kamil Samigullin <kamil@samigullin.info>
2019-03-23 20:20:34 +0300

    solve Maximum Value of the Loot problem

--- coursera/algorithmic-toolbox/week-3/greedy-algorithms/2_maximum_value_of_the_loot/solution.py
+++ coursera/algorithmic-toolbox/week-3/greedy-algorithms/2_maximum_value_of_the_loot/solution.py
@@ -1,17 +1,27 @@
-# Uses python3
-import sys
+# python3
+from typing import List
 
-def get_optimal_value(capacity, weights, values):
-    value = 0.
-    # write your code here
 
-    return value
+def get_optimal_value(cap: int, loots: List[tuple]):
+    optimal = 0.0
+
+    loots = sorted(loots, key=lambda x: x[0] / x[1], reverse=True)
+    while cap > 0 and len(loots) > 0:
+        best, loots = loots[0], loots[1:]
+        v, w = best
+        score = v / w
+        w = min(w, cap)
+        optimal += score * w
+        cap -= w
+
+    return optimal
 
 
 if __name__ == "__main__":
-    data = list(map(int, sys.stdin.read().split()))
-    n, capacity = data[0:2]
-    values = data[2:(2 * n + 2):2]
-    weights = data[3:(2 * n + 2):2]
-    opt_value = get_optimal_value(capacity, weights, values)
-    print("{:.10f}".format(opt_value))
+    n, capacity = list(map(int, input().split()))
+    data = []
+    for _ in range(n):
+        value, weight = map(int, input().split())
+        data.append((value, weight))
+    opt_value = get_optimal_value(capacity, data)
+    print("{:.4f}".format(opt_value))
