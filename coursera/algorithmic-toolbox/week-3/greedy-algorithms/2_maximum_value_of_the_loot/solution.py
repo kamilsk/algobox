36d1c1700d15c3e99a934dc0f1a77fa886cf7990
Kamil Samigullin <kamil@samigullin.info>
2019-03-24 12:41:28 +0300

    improve Maximum Value of the Loot solution

--- coursera/algorithmic-toolbox/week-3/greedy-algorithms/2_maximum_value_of_the_loot/solution.py
+++ coursera/algorithmic-toolbox/week-3/greedy-algorithms/2_maximum_value_of_the_loot/solution.py
@@ -1,20 +1,34 @@
 # python3
+from collections import namedtuple
 from typing import List
+from unittest import TestCase
+
+Loot = namedtuple('Loot', 'value weight')
 
 
-def get_optimal_value(cap: int, loots: List[tuple]) -> float:
+def get_optimal_value(cap: int, loots: List[Loot]) -> float:
     optimal = 0.0
 
-    loots = sorted(loots, key=lambda x: x[0] / x[1], reverse=True)
-    while cap > 0 and len(loots) > 0:
+    loots = sorted(loots, key=lambda x: x.value / x.weight, reverse=True)
+    while cap > 0 and loots:
         best, loots = loots[0], loots[1:]
-        v, w = best
-        score = v / w
-        w = min(w, cap)
+        score = best.value / best.weight
+        w = min(best.weight, cap)
         optimal += score * w
         cap -= w
 
     return optimal
+
+
+class Test(TestCase):
+    def test_get_optimal_value(self):
+        tests = [
+            {'cap': 50, 'loots': [Loot(60, 20), Loot(100, 50), Loot(120, 30)], 'expected': 180.0000},
+            {'cap': 10, 'loots': [Loot(500, 30)], 'expected': 166.6667},
+        ]
+        for test in tests:
+            expected, obtained = test['expected'], get_optimal_value(test['cap'], test['loots'])
+            self.assertEqual('{:.4f}'.format(expected), '{:.4f}'.format(obtained))
 
 
 if __name__ == '__main__':
@@ -22,6 +36,6 @@ if __name__ == '__main__':
     data = []
     for _ in range(n):
         value, weight = map(int, input().split())
-        data.append((value, weight))
+        data.append(Loot(value, weight))
     opt_value = get_optimal_value(capacity, data)
     print('{:.4f}'.format(opt_value))
