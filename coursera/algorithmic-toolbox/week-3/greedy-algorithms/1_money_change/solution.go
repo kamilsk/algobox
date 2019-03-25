19db935c5653232baf55bbf8d16a9171c29c006a
Kamil Samigullin <kamil@samigullin.info>
2019-03-25 22:36:39 +0300

    solve coursera/algorithmic-toolbox/week-3/greedy-algorithms/1_money_change at Go

--- coursera/algorithmic-toolbox/week-3/greedy-algorithms/1_money_change/solution.go
+++ coursera/algorithmic-toolbox/week-3/greedy-algorithms/1_money_change/solution.go
@@ -1,3 +1,28 @@
 package main
 
-func main() {}
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func getChange(m int64) int64 {
+	coins, pars := int64(0), []int64{10, 5, 1}
+	for _, par := range pars {
+		coins += m / par
+		m %= par
+	}
+	return coins
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+
+	_ = scanner.Scan()
+	n, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+	safe(fmt.Fprintln(os.Stdout, getChange(n)))
+}
+
+func safe(interface{}, error) {}
