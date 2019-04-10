1bd18caa2164e129a0dc16a886a736bab9dd0391
Kamil Samigullin <kamil@samigullin.info>
2019-04-10 23:17:44 +0300

    solve coursera/algorithmic-toolbox/week-4/divide-and-conquer/1_binary_search

--- coursera/algorithmic-toolbox/week-4/divide-and-conquer/1_binary_search/solution.py
+++ coursera/algorithmic-toolbox/week-4/divide-and-conquer/1_binary_search/solution.py
@@ -1,22 +1,40 @@
-# Uses python3
-import sys
+# python3
+from unittest import TestCase
 
-def binary_search(a, x):
-    left, right = 0, len(a)
-    # write your code here
+from sys import stdin
+from typing import List
 
-def linear_search(a, x):
-    for i in range(len(a)):
-        if a[i] == x:
-            return i
+
+def binary_search(where: List[int], what: int) -> int:
+    left, right = 0, len(where) - 1
+    while left <= right:
+        middle = (left + right) // 2
+        if what == where[middle]:
+            return middle
+        if what > where[middle]:
+            left = middle + 1
+            continue
+        right = middle - 1
     return -1
 
+
+class Test(TestCase):
+    def test_binary_search(self):
+        tests = [
+            {'where': [1, 5, 8, 12, 13], 'what': [8, 1, 23, 1, 11], 'expected': [2, 0, -1, 0, -1]},
+            {'where': [1, 2, 3, 4, 5], 'what': [1, 2, 3, 4, 5], 'expected': [0, 1, 2, 3, 4]},
+        ]
+        for test in tests:
+            obtained = []
+            for what in test['what']:
+                obtained.append(binary_search(test['where'], what))
+            self.assertEqual(test['expected'], obtained)
+
+
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
+    data = list(map(int, stdin.read().split()))
     n = data[0]
     m = data[n + 1]
-    a = data[1 : n + 1]
+    a = data[1: n + 1]
     for x in data[n + 2:]:
-        # replace with the call to binary_search when implemented
-        print(linear_search(a, x), end = ' ')
+        print(binary_search(a, x), end=' ')
