5fde30058892702c538d9f137ba0b3694f15687a
Kamil Samigullin <kamil@samigullin.info>
2019-04-11 15:47:45 +0300

    prepare solution for coursera/algorithmic-toolbox/week-4/divide-and-conquer/2_majority_element

--- coursera/algorithmic-toolbox/week-4/divide-and-conquer/1_binary_search/solution.py
+++ coursera/algorithmic-toolbox/week-4/divide-and-conquer/1_binary_search/solution.py
@@ -1,8 +1,11 @@
 # python3
+from collections import namedtuple
 from unittest import TestCase
 
 from sys import stdin
 from typing import List
+
+testCase = namedtuple('test', 'where what expected')
 
 
 def binary_search(where: List[int], what: int) -> int:
@@ -21,20 +24,19 @@ def binary_search(where: List[int], what: int) -> int:
 class Test(TestCase):
     def test_binary_search(self):
         tests = [
-            {'where': [1, 5, 8, 12, 13], 'what': [8, 1, 23, 1, 11], 'expected': [2, 0, -1, 0, -1]},
-            {'where': [1, 2, 3, 4, 5], 'what': [1, 2, 3, 4, 5], 'expected': [0, 1, 2, 3, 4]},
+            testCase([1, 5, 8, 12, 13], [8, 1, 23, 1, 11], [2, 0, -1, 0, -1]),
+            testCase([1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [0, 1, 2, 3, 4]),
         ]
         for test in tests:
             obtained = []
-            for what in test['what']:
-                obtained.append(binary_search(test['where'], what))
-            self.assertEqual(test['expected'], obtained)
+            for what in test.what:
+                obtained.append(binary_search(test.where, what))
+            self.assertEqual(test.expected, obtained)
 
 
 if __name__ == '__main__':
     data = list(map(int, stdin.read().split()))
     n = data[0]
-    m = data[n + 1]
-    a = data[1: n + 1]
+    m, a = data[n + 1], data[1: n + 1]
     for x in data[n + 2:]:
         print(binary_search(a, x), end=' ')
--- coursera/algorithmic-toolbox/week-4/divide-and-conquer/2_majority_element/solution.py
+++ coursera/algorithmic-toolbox/week-4/divide-and-conquer/2_majority_element/solution.py
@@ -1,18 +1,32 @@
-# Uses python3
-import sys
+# python3
+from collections import namedtuple
+from unittest import TestCase
 
-def get_majority_element(a, left, right):
+from sys import stdin
+from typing import List
+
+testCase = namedtuple('test', 'votes expected')
+
+
+def majority_element(votes: List[int], left: int, right: int) -> int:
     if left == right:
         return -1
     if left + 1 == right:
-        return a[left]
-    #write your code here
+        return votes[left]
     return -1
 
+
+class Test(TestCase):
+    def test_majority_element(self):
+        tests = [
+            testCase([2, 3, 9, 2, 2], 2),
+            testCase([1, 2, 3, 4], -1),
+            testCase([1, 2, 3, 1], -1),
+        ]
+        for test in tests:
+            self.assertEqual(test.expected, majority_element(test.votes, 0, len(test.votes)))
+
+
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    n, *a = list(map(int, input.split()))
-    if get_majority_element(a, 0, n) != -1:
-        print(1)
-    else:
-        print(0)
+    n, *a = list(map(int, stdin.read().split()))
+    print(1 if majority_element(a, 0, n) != -1 else 0)
