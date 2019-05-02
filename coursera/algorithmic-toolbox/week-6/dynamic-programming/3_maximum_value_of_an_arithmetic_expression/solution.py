30e673cdaab9f95f6584e564527f856f42afb9b1
Kamil Samigullin <kamil@samigullin.info>
2019-05-02 22:13:34 +0300

    solve coursera/algorithmic-toolbox/week-6/dynamic-programming/3_maximum_value_of_an_arithmetic_expression

--- coursera/algorithmic-toolbox/week-6/dynamic-programming/3_maximum_value_of_an_arithmetic_expression/solution.py
+++ coursera/algorithmic-toolbox/week-6/dynamic-programming/3_maximum_value_of_an_arithmetic_expression/solution.py
@@ -1,5 +1,11 @@
-# Uses python3
-def evalt(a, b, op):
+# python3
+from collections import namedtuple
+from unittest import TestCase
+
+test = namedtuple('test', 'expression expected')
+
+
+def calculate(a: int, b: int, op: chr) -> int:
     if op == '+':
         return a + b
     elif op == '-':
@@ -9,10 +15,41 @@ def evalt(a, b, op):
     else:
         assert False
 
-def get_maximum_value(dataset):
-    #write your code here
-    return 0
+
+def maximum_value(expression: str) -> int:
+    n = 1 + len(expression) // 2
+    mins, maxs = [], []
+    for i in range(n):
+        mins.append([0] * n)
+        maxs.append([0] * n)
+        mins[i][i] = maxs[i][i] = int(expression[2 * i])
+
+    for s in range(1, n):
+        for i in range(n - s):
+            j = i + s
+            cur_min, cur_max = 9 ** 14, -9 ** 14
+            for k in range(i, j):
+                a = calculate(maxs[i][k], maxs[k + 1][j], expression[2 * k + 1])
+                b = calculate(maxs[i][k], mins[k + 1][j], expression[2 * k + 1])
+                c = calculate(mins[i][k], maxs[k + 1][j], expression[2 * k + 1])
+                d = calculate(mins[i][k], mins[k + 1][j], expression[2 * k + 1])
+                cur_min = min(cur_min, a, b, c, d)
+                cur_max = max(cur_max, a, b, c, d)
+            mins[i][j], maxs[i][j] = cur_min, cur_max
+
+    return maxs[0].pop()
 
 
-if __name__ == "__main__":
-    print(get_maximum_value(input()))
+class Test(TestCase):
+    def test_maximum_value(self):
+        tests = [
+            # samples
+            test('1+5', 6),
+            test('5-8+7*4-8+9', 200),
+        ]
+        for i, t in enumerate(tests):
+            self.assertEqual(t.expected, maximum_value(t.expression), msg='at {} position'.format(i))
+
+
+if __name__ == '__main__':
+    print(maximum_value(input()))
