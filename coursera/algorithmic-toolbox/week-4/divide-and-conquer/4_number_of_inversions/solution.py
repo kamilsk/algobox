c16d5dc8dfb5a517de8ce3319cf8becfc505991e
Kamil Samigullin <kamil@samigullin.info>
2019-05-04 11:58:21 +0300

    try to solve coursera/algorithmic-toolbox/week-4/divide-and-conquer/4_number_of_inversions: first failed iteration

--- coursera/algorithmic-toolbox/week-4/divide-and-conquer/4_number_of_inversions/solution.py
+++ coursera/algorithmic-toolbox/week-4/divide-and-conquer/4_number_of_inversions/solution.py
@@ -1,18 +1,61 @@
-# Uses python3
-import sys
+# python3
+from collections import namedtuple
+from sys import stdin
+from typing import List
+from unittest import TestCase
 
-def get_number_of_inversions(a, b, left, right):
-    number_of_inversions = 0
+test = namedtuple('test', 'seq expected')
+
+
+def number_of_inversions(seq: List[int], buf: List[int], left: int, right: int) -> int:
+    count = 0
     if right - left <= 1:
-        return number_of_inversions
+        return count
     ave = (left + right) // 2
-    number_of_inversions += get_number_of_inversions(a, b, left, ave)
-    number_of_inversions += get_number_of_inversions(a, b, ave, right)
-    #write your code here
-    return number_of_inversions
+    count += number_of_inversions(seq, buf, left, ave)
+    count += number_of_inversions(seq, buf, ave, right)
+
+    i, j = left, ave
+    while i < ave or j < right:
+        if i == ave:
+            buf[i + j - ave] = seq[j]
+            j += 1
+            continue
+        if j == right:
+            buf[i + j - ave] = seq[i]
+            i += 1
+            count += 1
+            continue
+        if seq[i] <= seq[j]:
+            buf[i + j - ave] = seq[i]
+            i += 1
+        else:
+            buf[i + j - ave] = seq[j]
+            j += 1
+            count += 1
+
+    seq[left:right] = buf[left:right]
+    return count
+
+
+class Test(TestCase):
+    def test_number_of_inversions(self):
+        tests = [
+            # samples
+            test([2, 3, 9, 2, 9], 2),
+
+            # acceptance
+            test([9, 8, 7, 3, 2, 1], 15),
+
+            # additional
+            test([1, 2, 3, 4, 5], 0),
+            test([5, 4, 3, 2, 1], 11),
+        ]
+        for i, t in enumerate(tests):
+            self.assertEqual(t.expected, number_of_inversions(t.seq, [0] * len(t.seq), 0, len(t.seq)),
+                             msg='at {} position'.format(i))
+
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    n, *a = list(map(int, input.split()))
-    b = n * [0]
-    print(get_number_of_inversions(a, b, 0, len(a)))
+    n, *a = list(map(int, stdin.read().split()))
+    print(number_of_inversions(a, n * [0], 0, len(a)))
