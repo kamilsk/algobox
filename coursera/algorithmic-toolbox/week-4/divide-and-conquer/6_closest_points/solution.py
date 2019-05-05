4c279355dee0a06c8a88beb4b11ed2cd4b7a8626
Kamil Samigullin <kamil@samigullin.info>
2019-05-05 13:13:59 +0300

    implement naive algorithm for coursera/algorithmic-toolbox/week-4/divide-and-conquer/6_closest_points

--- coursera/algorithmic-toolbox/week-4/divide-and-conquer/6_closest_points/solution.py
+++ coursera/algorithmic-toolbox/week-4/divide-and-conquer/6_closest_points/solution.py
@@ -1,14 +1,39 @@
 # python3
 from collections import namedtuple
+from math import sqrt
 from sys import stdin
 from typing import List
 from unittest import TestCase
 
+point = namedtuple('point', 'x y')
 test = namedtuple('test', 'x y expected')
 
 
-def minimum_distance(x: List[int], y: List[int]) -> int:
-    return 10 ** 18
+def closest_pair(points: List[point]) -> (point, point):
+    return None, None
+
+
+def closes_split_pair(points: List[point], delta: float) -> (point, point):
+    return None, None
+
+
+def distance(p1: point, p2: point) -> float:
+    return sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2)
+
+
+def naive_minimum_distance(x: List[int], y: List[int]) -> float:
+    points = [point(x[i], y[i]) for i in range(len(x))]
+
+    result = distance(point(-10 ** 9, -10 ** 9), point(10 ** 9, 10 ** 9))
+    for i in range(len(points)):
+        for j in range(i + 1, len(points)):
+            result = min(result, distance(points[i], points[j]))
+
+    return result
+
+
+def fast_minimum_distance(x: List[int], y: List[int]) -> float:
+    pass
 
 
 class Test(TestCase):
@@ -20,10 +45,11 @@ class Test(TestCase):
             test([4, -2, -3, -1, 2, -4, 1, -1, 3, -4, -2], [4, -2, -4, 3, 3, 0, 1, -1, -1, 2, 4], 1.414213),
         ]
         for i, t in enumerate(tests):
-            self.assertEqual(t.expected, minimum_distance(t.x, t.y), msg='at {} position'.format(i))
+            self.assertEqual(t.expected, int(10 ** 6 * naive_minimum_distance(t.x, t.y)) / 10 ** 6,
+                             msg='at {} position'.format(i))
 
 
 if __name__ == '__main__':
     data = list(map(int, stdin.read().split()))
     n = data[0]
-    print("{0:.9f}".format(minimum_distance(data[1::2], data[2::2])))
+    print('{0:.9f}'.format(fast_minimum_distance(data[1::2], data[2::2])))
