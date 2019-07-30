5598917483813d40ed7628bad61d2018d1d6f8db
Kamil Samigullin <kamil@samigullin.info>
2019-07-30 16:17:30 +0300

    solve coursera/data-structures/week-4/1_phone_book

--- coursera/data-structures/week-4/1_phone_book/phone_book.py
+++ coursera/data-structures/week-4/1_phone_book/phone_book.py
@@ -1,47 +1,156 @@
 # python3
 
+from collections import namedtuple
+from unittest import TestCase
+
+from random import randint
+from typing import Any, Callable, List, Optional
+
+contact = namedtuple('contact', 'phone name')
+test = namedtuple('test', 'commands expected')
+
+
+class Node:
+    def __init__(self, value):
+        self.next = self.prev = None
+        self.value = value
+
+
+class LinkedList:
+    def __init__(self, compare: Callable[[Any, Any], bool]):
+        self.head = self.tail = None
+        self.compare = compare
+
+    def add(self, value):
+        if self.tail is None:
+            self.head = self.tail = Node(value)
+            return
+        found = self.find(value)
+        if found is None:
+            n, t = Node(value), self.tail
+            t.next, n.prev = n, t
+            self.tail = n
+            return
+        found.value = value
+
+    def remove(self, value):
+        found = self.find(value)
+        if found is None:
+            return
+        p, n = found.prev, found.next
+        if p is None and n is None:
+            self.head = self.tail = None
+            return
+        if p is None:
+            self.head = n
+            n.prev = None
+            return
+        if n is None:
+            self.tail = p
+            p.next = None
+            return
+        p.next, n.prev = n, p
+
+    def find(self, value) -> Optional[Node]:
+        if self.head is None:
+            return None
+        found = self.head
+        while found is not None:
+            if self.compare(found.value, value):
+                break
+            found = found.next
+        return found
+
+
 class Query:
     def __init__(self, query):
-        self.type = query[0]
-        self.number = int(query[1])
+        self.type, self.phone = query[0], int(query[1])
         if self.type == 'add':
             self.name = query[2]
 
-def read_queries():
-    n = int(input())
-    return [Query(input().split()) for i in range(n)]
 
-def write_responses(result):
-    print('\n'.join(result))
+class PhoneBook:
+    @staticmethod
+    def __universal_family(cardinality: int) -> Callable[[int], int]:
+        primary = 10 ** 7 + randint(10, 100)
+        a, b = randint(1, primary - 1), randint(0, primary - 1)
+        return lambda x: ((a * x + b) % primary) % cardinality
 
-def process_queries(queries):
-    result = []
-    # Keep list of all existing (i.e. not deleted yet) contacts.
-    contacts = []
-    for cur_query in queries:
-        if cur_query.type == 'add':
-            # if we already have contact with such number,
-            # we should rewrite contact's name
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    contact.name = cur_query.name
-                    break
-            else: # otherwise, just add it
-                contacts.append(cur_query)
-        elif cur_query.type == 'del':
-            for j in range(len(contacts)):
-                if contacts[j].number == cur_query.number:
-                    contacts.pop(j)
-                    break
-        else:
+    def __init__(self):
+        cardinality = 10 ** 7 // 10 ** 4
+        self.__hash = self.__universal_family(cardinality)
+        self.__storage: List[Optional[LinkedList]] = [None] * cardinality
+
+    def process(self, queries: List[str]) -> List[str]:
+        result = []
+
+        for query in [Query(query.split()) for query in queries]:
+            hh = self.__hash(query.phone)
+            if self.__storage[hh] is None:
+                self.__storage[hh] = LinkedList(lambda x, y: x.phone == y.phone)
+            ll = self.__storage[hh]
+
+            if query.type == 'add':
+                ll.add(contact(query.phone, query.name))
+                continue
+
+            if query.type == 'del':
+                ll.remove(contact(query.phone, None))
+                continue
+
             response = 'not found'
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    response = contact.name
-                    break
+            c = ll.find(contact(query.phone, None))
+            if c is not None:
+                response = c.value.name
             result.append(response)
-    return result
+
+        return result
+
+
+class Test(TestCase):
+    def test_phone_book(self):
+        tests = [
+            # samples
+            test([
+                'add 911 police',
+                'add 76213 Mom',
+                'add 17239 Bob',
+                'find 76213',
+                'find 910',
+                'find 911',
+                'del 910',
+                'del 911',
+                'find 911',
+                'find 76213',
+                'add 76213 daddy',
+                'find 76213',
+            ], [
+                'Mom',
+                'not found',
+                'police',
+                'not found',
+                'Mom',
+                'daddy',
+            ]),
+            test([
+                'find 3839442',
+                'add 123456 me',
+                'add 0 granny',
+                'find 0',
+                'find 123456',
+                'del 0',
+                'del 0',
+                'find 0',
+            ], [
+                'not found',
+                'granny',
+                'me',
+                'not found',
+            ]),
+        ]
+        for i, t in enumerate(tests):
+            self.assertEqual(t.expected, PhoneBook().process(t.commands))
+
 
 if __name__ == '__main__':
-    write_responses(process_queries(read_queries()))
-
+    print('\n'.join(PhoneBook().process([input() for _ in range(int(input()))])))
